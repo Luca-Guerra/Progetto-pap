67d48d12b3c761d30d3f3a0c155f0c6458937e1b
guerra <luca.guerra.brg@gmail.com>
2014-05-10 18:56:36 +0200

    prima bozza.

--- DocumentIndexing/src/base/test.java
+++ DocumentIndexing/src/base/test.java
@@ -1,9 +1,20 @@
 package base;
 
+import java.util.Hashtable;
+
 public class test {
 
 	public static void main(String[] args)
 	{
 		System.out.println("Hello World");
+		//Creo l'hashtable
+		Hashtable<String, String[]> DocIndex = new Hashtable<String, String[]>();
+		//Ciclo le varie directory
+		//per ogni file trovato aggiorno la hashtable
+		
+		
+		
+		//date le parole eseguo le query per determinare in quali documenti sono presenti le varie parole passate
 	}
+	
 }
