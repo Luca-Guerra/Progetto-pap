dcf8f43f5818d3320882108487669f05f3eaaeb6
guerra <luca.guerra.brg@gmail.com>
2014-05-10 19:00:50 +0200

    Aggiunti import per GUI

---
+++ DocumentIndexing/src/base/DocIndexing.java
@@ -0,0 +1,24 @@
+package base;
+
+import java.util.Hashtable;
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;  //notice javax
+
+public class DocIndexing {
+
+	private Hashtable<String, String[]> DocIndex = new Hashtable<String, String[]>();
+	
+	public static void main(String[] args)
+	{
+		System.out.println("Hello World");
+		
+		//Ciclo le varie directory
+		//per ogni file trovato aggiorno la hashtable
+		
+		
+		
+		//date le parole eseguo le query per determinare in quali documenti sono presenti le varie parole passate
+	}
+	
+}
